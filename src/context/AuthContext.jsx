a481b934e9963b648550ae53eb85745b436593c7
Shiwam Tiwari <zaqqst@gmail.com>
2022-06-20 21:59:47 +0530

    context working

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext } from "react";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(false);
 
 export const AuthProvider = ({ children }) => {
   // code here
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
-  return <div>{"login"}</div>;
+  const isLogin = useContext(AuthContext)
+  return <div>{isLogin?"logout":"login"}</div>;
 };
 
 export default Login;
